2219954e0e0b90eacbbc2d6aad2d65045fd049a9
Koloman Mautner <kmautner@c2r7p2.42vienna.com>
2025-04-09 16:15:59 +0200

    replaced long string equasion in the if statements with a single call to str_equals()

--- builtins_pipes.c
+++ builtins_pipes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_pipes.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kfan <kfan@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: kmautner <kmautner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 14:25:47 by kfan              #+#    #+#             */
-/*   Updated: 2025/04/09 14:24:59 by kfan             ###   ########.fr       */
+/*   Updated: 2025/04/09 16:15:24 by kmautner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 // pipes for builtins
 int	builtins_pipe_fd_out(t_cmds *cmds, int *fd)
 {
-	if (fd && ((!ft_strncmp(cmds->cmd[0], "echo", 4)
-				&& ft_strlen(cmds->cmd[0]) == 4) || (!ft_strncmp(cmds->cmd[0],
-					"cd", 2) && ft_strlen(cmds->cmd[0]) == 2)
-			|| (!ft_strncmp(cmds->cmd[0], "pwd", 3)
-				&& ft_strlen(cmds->cmd[0]) == 3) || (!ft_strncmp(cmds->cmd[0],
-					"export", 6) && ft_strlen(cmds->cmd[0]) == 6)
-			|| (!ft_strncmp(cmds->cmd[0], "unset", 5)
-				&& ft_strlen(cmds->cmd[0]) == 5) || (!ft_strncmp(cmds->cmd[0],
-					"env", 3) && ft_strlen(cmds->cmd[0]) == 3)
-			|| (!ft_strncmp(cmds->cmd[0], "exit", 4)
-				&& ft_strlen(cmds->cmd[0]) == 4)))
+	if (fd && (str_equals(cmds->cmd[0], "echo")
+			|| str_equals(cmds->cmd[0], "cd")
+			|| str_equals(cmds->cmd[0], "pwd")
+			|| str_equals(cmds->cmd[0], "export")
+			|| str_equals(cmds->cmd[0], "unset")
+			|| str_equals(cmds->cmd[0], "env")
+			|| str_equals(cmds->cmd[0], "exit")))
 	{
 		if (!cmds->outfile && dup2(fd[1], 1) == -1)
 		{
@@ -40,17 +36,13 @@ int	builtins_pipe_fd_out(t_cmds *cmds, int *fd)
 
 int	builtins_pipe_fd_in(t_cmds *cmds, int *fd)
 {
-	if (fd && ((!ft_strncmp(cmds->cmd[0], "echo", 4)
-				&& ft_strlen(cmds->cmd[0]) == 4) || (!ft_strncmp(cmds->cmd[0],
-					"cd", 2) && ft_strlen(cmds->cmd[0]) == 2)
-			|| (!ft_strncmp(cmds->cmd[0], "pwd", 3)
-				&& ft_strlen(cmds->cmd[0]) == 3) || (!ft_strncmp(cmds->cmd[0],
-					"export", 6) && ft_strlen(cmds->cmd[0]) == 6)
-			|| (!ft_strncmp(cmds->cmd[0], "unset", 5)
-				&& ft_strlen(cmds->cmd[0]) == 5) || (!ft_strncmp(cmds->cmd[0],
-					"env", 3) && ft_strlen(cmds->cmd[0]) == 3)
-			|| (!ft_strncmp(cmds->cmd[0], "exit", 4)
-				&& ft_strlen(cmds->cmd[0]) == 4)))
+	if (fd && (str_equals(cmds->cmd[0], "echo")
+			|| str_equals(cmds->cmd[0], "cd")
+			|| str_equals(cmds->cmd[0], "pwd")
+			|| str_equals(cmds->cmd[0], "export")
+			|| str_equals(cmds->cmd[0], "unset")
+			|| str_equals(cmds->cmd[0], "env")
+			|| str_equals(cmds->cmd[0], "exit")))
 	{
 		if (dup2(fd[0], 0) == -1)
 		{
